6ac456ed9a0c2a6bcd8b79d8209976f7dca15dda
Sasha <q.dev.null@gmail.com>
2022-06-03 12:24:07 +0100

    Fix the contract name

--- test/index.ts
+++ test/index.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
+    const Greeter = await ethers.getContractFactory("Prenoms");
     const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
 
