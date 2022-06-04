c63694d6b41f7d16d273fb87da9ceee7a397b489
kqf <q.dev.null@gmail.com>
2022-06-04 04:40:55 +0100

    Switch to erc721 (#10)
    * Modify the contract
    
    * Fix the tests
    
    * Add more test cases

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,19 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+describe("Minter", function () {
+  it("Should mint a token with a given URI", async function () {
+    // Create a phony account
+    const [owner] = await ethers.getSigners();
+
     const Prenoms = await ethers.getContractFactory("Prenoms");
-    const prenoms = await Prenoms.deploy("Hello, world!");
+    const prenoms = await Prenoms.deploy();
     await prenoms.deployed();
 
-    expect(await prenoms.greet()).to.equal("Hello, world!");
+    await prenoms.safeMint(owner.address, "Hello, world!");
+    expect(await prenoms.tokenURI(0)).to.equal("Hello, world!");
 
-    const setGreetingTx = await prenoms.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await prenoms.greet()).to.equal("Hola, mundo!");
+    await prenoms.safeMint(owner.address, "Second token");
+    expect(await prenoms.tokenURI(1)).to.equal("Second token");
   });
 });
