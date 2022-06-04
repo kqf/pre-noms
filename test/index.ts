602391ea828a0c68cdfc392f65d7f2c30862c6bb
kqf <q.dev.null@gmail.com>
2022-06-04 04:06:54 +0100

    Modify the contract (#8)
    * Rename the file
    
    * Fix the class name
    
    * Fix the contract name
    
    * Fix the tests

--- test/index.ts
+++ test/index.ts
@@ -3,17 +3,17 @@ import { ethers } from "hardhat";
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const Prenoms = await ethers.getContractFactory("Prenoms");
+    const prenoms = await Prenoms.deploy("Hello, world!");
+    await prenoms.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    expect(await prenoms.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const setGreetingTx = await prenoms.setGreeting("Hola, mundo!");
 
     // wait until the transaction is mined
     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await prenoms.greet()).to.equal("Hola, mundo!");
   });
 });
