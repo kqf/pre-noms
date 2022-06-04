9e28c214c5b204ec3d185f4b722b832e4a191795
Sasha <q.dev.null@gmail.com>
2022-06-04 04:38:02 +0100

    Fix the tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,15 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+describe("Minter", function () {
+  it("Should mint a token with a given URI", async function () {
+    // Create a phony account
+    const [owner] = await ethers.getSigners();
+
     const Prenoms = await ethers.getContractFactory("Prenoms");
-    const prenoms = await Prenoms.deploy("Hello, world!");
+    const prenoms = await Prenoms.deploy();
     await prenoms.deployed();
-
-    expect(await prenoms.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await prenoms.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await prenoms.greet()).to.equal("Hola, mundo!");
+    await prenoms.safeMint(owner.address, "Hello, world!");
+    expect(await prenoms.tokenURI(0)).to.equal("Hello, world!");
   });
 });
