199774b5875cc8ff432c41975fc40e60839d3a84
Sasha <q.dev.null@gmail.com>
2022-06-04 04:39:15 +0100

    Add more test cases

--- test/index.ts
+++ test/index.ts
@@ -9,7 +9,11 @@ describe("Minter", function () {
     const Prenoms = await ethers.getContractFactory("Prenoms");
     const prenoms = await Prenoms.deploy();
     await prenoms.deployed();
+
     await prenoms.safeMint(owner.address, "Hello, world!");
     expect(await prenoms.tokenURI(0)).to.equal("Hello, world!");
+
+    await prenoms.safeMint(owner.address, "Second token");
+    expect(await prenoms.tokenURI(1)).to.equal("Second token");
   });
 });
