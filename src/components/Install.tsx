a2770138e5b9f9ed2cf1fbe27cff974b1b40ba63
Sasha <q.dev.null@gmail.com>
2022-06-09 08:20:33 +0200

    Center the text

--- src/components/Install.tsx
+++ src/components/Install.tsx
@@ -1,6 +1,6 @@
 const Install = () => {
   return (
-    <div>
+    <div className="text-center">
       <h3>Please follow the link to install the wallet</h3>
       <a href="https://metamask.io/download.html">Meta Mask</a>
     </div>
