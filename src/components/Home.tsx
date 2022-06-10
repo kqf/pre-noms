2abe92b6ff875f03c7369cb6f3c8ab1bb8329e4b
Sasha <q.dev.null@gmail.com>
2022-06-10 21:21:06 +0200

    Add the home boilerplate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,36 @@
-const Home = () => {
-    return (
-      <div>
-            Hello world!
-      </div>
-    );
+function Entry() {
+  return <div></div>
+}
+
+function Home() {
+
+  const [totalMinted, setTotalMinted] = useState(0);
+  useEffect(() => {
+    getCount();
+  }, []);
+
+  const getCount = async () => {
+    const count = await contract.count();
+    console.log(parseInt(count));
+    setTotalMinted(parseInt(count));
   };
 
-  export default Home;
+  return (
+    <div>
+      <WalletBalance />
+
+      <h1>This is a text</h1>
+      <div className="container">
+        <div className="row">
+          {Array(totalMinted + 1)
+            .fill(0)
+            .map((_, i) => (
+              <div key={i} className="col-sm">
+                <Entry tokenId={i} getCount={getCount} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
