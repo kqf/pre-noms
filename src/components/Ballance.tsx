f8a2e9929cd9e654e9fbc0e5f7aa9e8fc5d9edbd
kqf <q.dev.null@gmail.com>
2022-06-10 21:14:45 +0200

    Add wallet balance (#23)
    * Add a code for button
    
    * Retrieve wallet ballance

---
+++ src/components/Ballance.tsx
@@ -0,0 +1,32 @@
+
+
+import { useState } from 'react';
+import { ethers } from 'ethers';
+
+function WalletBalance() {
+
+    const [balance, setBalance] = useState();
+
+    const getBalance = async () => {
+        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+        const balance = await provider.getBalance(account as any);
+        setBalance(ethers.utils.formatEther(balance) as any);
+    };
+
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Your Balance: {balance}</h5>
+                <button
+                    className="btn btn-success"
+                    onClick={() => getBalance()}
+                >
+                    Show My Balance
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default WalletBalance;
