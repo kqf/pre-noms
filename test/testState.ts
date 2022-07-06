f9120bcd04486641cc1faeacd0c3f34bc948ce47
Sasha <q.dev.null@gmail.com>
2022-07-06 18:44:08 +0200

    Fix the tests

--- test/testState.ts
+++ test/testState.ts
@@ -1,5 +1,8 @@
+import {buildStore } from "../src/store"
+
 describe("Check the store logic", function () {
     it("Should mint a token with a given URI", async function () {
+        const store = buildStore()
         console.log("Done");
     });
 });
