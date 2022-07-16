5b25538b0d1bccfd186a8430a1a0ab2e51b9b1f9
Sasha <q.dev.null@gmail.com>
2022-07-16 07:22:07 +0100

    Add more checks

--- test/testState.ts
+++ test/testState.ts
@@ -1,3 +1,4 @@
+import { expect } from "chai";
 import { ethers } from "hardhat";
 import { fetchAllTokens, mintToken } from "../src/store/collection";
 import { buildStore } from "../src/store/store";
@@ -12,10 +13,13 @@ describe("Check the store logic", function () {
         await prenoms.deployed();
         const store = buildStore()
 
+        await prenoms.donateMint(owner.address, "dummy url");
         // @ts-ignore
         await store.dispatch(fetchAllTokens(prenoms));
         // @ts-ignore
         await store.dispatch(mintToken({contract: prenoms, signer: owner}));
+        console.log(store.getState());
+        // expect(store.getState().collection.lastId).to.equal(1);
         console.log("Done");
     });
 })
