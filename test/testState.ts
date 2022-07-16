64b35a81718b71ef76376d9776709b74d49f3041
kqf <q.dev.null@gmail.com>
2022-07-16 07:12:33 +0100

    Check mints (#52)
    * Check for completeness
    
    * Fix the selector
    
    * Fix the condition
    
    * Add boilerplate code to mint to ken
    
    * Fix typings
    
    * Fix the collections
    
    * Add mint reducer
    
    * Check if mints
    
    * Fix the test state

--- test/testState.ts
+++ test/testState.ts
@@ -1,17 +1,21 @@
 import { ethers } from "hardhat";
-import { fetchAllTokens } from "../src/store/collection";
+import { fetchAllTokens, mintToken } from "../src/store/collection";
 import { buildStore } from "../src/store/store";
 
 
 describe("Check the store logic", function () {
     it("Interact with the store", async function () {
         // Create a phony account
+        const [owner] = await ethers.getSigners();
         const Prenoms = await ethers.getContractFactory("Prenoms");
         const prenoms = await Prenoms.deploy();
         await prenoms.deployed();
         const store = buildStore()
+
         // @ts-ignore
         await store.dispatch(fetchAllTokens(prenoms));
+        // @ts-ignore
+        await store.dispatch(mintToken({contract: prenoms, signer: owner}));
         console.log("Done");
     });
-});
+})
