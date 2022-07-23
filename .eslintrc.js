a01587cb81e02e41089d91212a6b5dfa8d3a2b93
Sasha <q.dev.null@gmail.com>
2022-07-23 07:46:30 +0100

    Add indentation explicit rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    mocha: true,
-    node: true
-  },
-  root: true,
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12
-  },
-  rules: {
-    "@typescript-eslint/no-explicit-any": ["off"],
-    "semi": ["error", "always"],
-  }
+    env: {
+        browser: false,
+        es2021: true,
+        mocha: true,
+        node: true
+    },
+    root: true,
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {
+        "@typescript-eslint/no-explicit-any": ["off"],
+        "semi": ["error", "always"],
+        "indent": ["error", 4],
+    }
 };
