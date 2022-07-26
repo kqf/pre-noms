bd5e8278e5a03b6e71e867a7782a44a6215b34f7
kqf <q.dev.null@gmail.com>
2022-07-26 08:10:17 +0100

    Add fixtures like (#73)
    * Fix main fixtures
    
    * Add the missing typings

--- test/testContract.ts
+++ test/testContract.ts
@@ -1,15 +1,21 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Minter", function () {
+    let owner: SignerWithAddress;
+    let prenoms: Contract;
+
+    beforeEach(async () => {
+        [owner] = await ethers.getSigners();
+        const Prenoms = await ethers.getContractFactory("Prenoms");
+        prenoms = await Prenoms.deploy();
+        await prenoms.deployed();
+    });
+
     it("Should mint a token with a given URI", async function () {
     // Create a phony account
-        const [owner] = await ethers.getSigners();
-
-        const Prenoms = await ethers.getContractFactory("Prenoms");
-        const prenoms = await Prenoms.deploy();
-        await prenoms.deployed();
-
         await prenoms.donateMint(owner.address, "Hello, world!");
         expect(await prenoms.tokenURI(0)).to.equal("ipfs://Hello, world!");
 
